c9ad1516fcdc981cb0e7fb08f75dce2cda90302d
桔子 <juzi214032@qq.com>
2020-07-03 02:02:56 +0800

    feat: 添加友盟统计

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,17 +3,25 @@ module.exports = {
   dest: "./dist",
   title: "Groovy 中文文档",
   description: "Groovy 中文文档",
+  head: [
+    ["script", {
+      async: true,
+      type: "text/javascript",
+      src: "https://s9.cnzz.com/z_stat.php?id=1279042755&web_id=1279042755"
+    }]
+  ],
   themeConfig: {
+    repo: "juzi214032/groovy-chinese",
+    repoLabel: 'Github',
+    docsDir: 'docs',
+    editLinks: true,
+    editLinkText: '纠正错误',
     nav: [{
       text: "快速开始",
       link: "/getting-started/download-groovy.md"
     }, {
       text: "文档",
       link: "/language-specification/syntax"
-    }, {
-      text: "Github",
-      target: "_blank",
-      link: "https://github.com/juzi214032/groovy-chinese"
     }]
   }
 }
